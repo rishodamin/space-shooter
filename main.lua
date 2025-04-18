a7c300cac18f9a713ecdffd6af19971fd096ad89
rishodamin <daminrisho@gmail.com>
2025-04-18 09:27:10 +0530

    text render implementation and pause feature

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local love = require("love")
-local Player = require("Player")
+local Player = require("objects/Player")
+local Game = require("states/Game")
 
 
 function love.load()
@@ -8,12 +9,24 @@ function love.load()
 
     local show_debug = false
     _G.player = Player(show_debug)
+    _G.game = Game()
 end
 
 function love.keypressed(key)
-    if key == "up" then
-        _G.player.thrusting = true
+    if game.state.running then
+        if key == "up" then
+            _G.player.thrusting = true
+        end
+
+        if key == "escape" then
+            game:changeGameState("paused")
+        end
+    elseif game.state.paused then
+        if key == "escape" then
+            game:changeGameState("running")
+        end
     end
+    
 end
 
 function love.keyreleased(key)
@@ -25,11 +38,18 @@ end
 function love.update(dt)
     _G.mouse_x, _G.mouse_y = love.mouse.getPosition()
 
-    player:movePlayer()
+    if game.state.running then
+        player:movePlayer()
+    end
+    
 end
-
+  
 function love.draw()
-    player:draw()
+    if game.state.running or game.state.paused then
+        player:draw()
+        game:draw(game.state.paused)
+    end
+    
 
     love.graphics.setColor(1, 1, 1, 1)
    -- love.graphics.print(love.timer.getFPS(), 10, 10)
---
+++ states/Game.lua
@@ -0,0 +1,37 @@
+local Text = require("../components/Text")
+local love = require("love")
+
+function Game()
+    return {
+        state = {
+            menu = false,
+            paused = false,
+            running  = true,
+            ended = false
+        },
+
+        changeGameState = function (self, state)
+            self.state.menu = state == "menu"
+            self.state.paused = state == "paused"
+            self.state.running = state == "running"
+            self.state.ended = state == "ended"
+        end,
+
+        draw = function (self, faded)
+            if faded then
+                Text(
+                    "PAUSED",
+                    0,
+                    love.graphics.getHeight() * 0.4,
+                    "h1",
+                    false,
+                    false,
+                    love.graphics.getWidth(),
+                    "center"
+                ):draw()
+            end
+        end
+    }
+end
+
+return Game
